4493123c8b18efca97ae8746c568e5f28aa98135
GerardFerrerBirbe <gferrerbirbe@gmail.com>
2020-06-18 08:55:14 +0200

    primera part del tutorial microsoft docs

---
+++ AspNet_WebApi_GetStarted/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNet_WebApi_GetStarted.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string JobPosition { get; set; }
+        public double Salary { get; set; }
+    }
+    
+}
